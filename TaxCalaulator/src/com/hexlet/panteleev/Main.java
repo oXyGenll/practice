4bbb199d511247087e988b70c720c2e22804e48c
OXYGENLL <alex_kc@mail.ru>
2018-03-26 09:10:22 +0300

    repo

--- TaxCalaulator/src/com/hexlet/panteleev/Main.java
+++ TaxCalaulator/src/com/hexlet/panteleev/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         // write your code here
         System.out.println("Введите сумму: ");
         Scanner scan = new Scanner(System.in);
