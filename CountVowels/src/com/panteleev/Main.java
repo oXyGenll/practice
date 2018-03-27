abfcd1b58dafa55ba9efc6ef1bd9e9d95fc5ff50
OXYGENLL <alex_kc@mail.ru>
2018-03-27 09:47:37 +0300

    Count Vowels

--- CountVowels/src/com/panteleev/Main.java
+++ CountVowels/src/com/panteleev/Main.java
@@ -1,9 +1,37 @@
 package com.panteleev;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args)
-    {
-	// write your code here
+    public static void main(String[] args) {
+        System.out.println("Введите строку: ");
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+
+        char[] vowels = {'a', 'o', 'i', 'y', 'u', 'e'};
+
+        char[] chars = str.toCharArray();
+        int k = 0;
+        for (char aChar1 : chars) {
+            for (char vowel : vowels) {
+                if (aChar1 == vowel) {
+                    k++;
+                }
+            }
+        }
+
+        char[] spaces= {' '};
+        int m = 0;
+        for (char aChar : chars) {
+            for (char aSpaces : spaces) {
+                if (aChar == aSpaces) {
+                    m++;
+                }
+            }
+        }
+        System.out.println(str);
+        System.out.println("Колличество гласных букв: " + k);
+        System.out.println("Колличество пробелов: " + m);
     }
 }
